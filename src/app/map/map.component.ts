b6fb9c0d5dcaee32b63598ed71033d42a701161b
GeloBom <lghs60@hotmail.com>
2025-04-14 17:46:31 -0300

    feat(polygon): add PolygonComponent and polygonService

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,10 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 import { MapService } from '../services/map.service';
+import { PolygonComponent } from "../polygon/polygon.component.spec";
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
   styleUrl: './map.component.scss',
+  imports: [PolygonComponent],
 })
 export class MapComponent implements OnInit {
   constructor(public mapService: MapService) {}
---
+++ src/app/polygon/polygon.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-polygon',
+  imports: [],
+  templateUrl: './polygon.component.html',
+  styleUrl: './polygon.component.scss'
+})
+export class PolygonComponent {
+
+}
