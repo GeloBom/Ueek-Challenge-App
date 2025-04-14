2f428934545b23b4997aaa635bca1ee69cf436d0
GeloBom <lghs60@hotmail.com>
2025-04-14 13:49:37 -0300

    test(map): add unit tests for zoomIn and zoomOut methods in MapComponent

--- src/app/map/map.component.spec.ts
+++ src/app/map/map.component.spec.ts
@@ -1,5 +1,4 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { MapComponent } from './map.component';
 
 describe('MapComponent', () => {
@@ -18,5 +17,25 @@ describe('MapComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call view.animate when zoomIn is called with map initialized', () => {
+    const viewSpy = jasmine.createSpyObj('View', ['getZoom', 'animate']);
+    spyOn(component.map, 'getView').and.returnValue(viewSpy);
+    viewSpy.getZoom.and.returnValue(5);
+
+    component.zoomIn();
+
+    expect(viewSpy.animate).toHaveBeenCalledWith({ zoom: 6, duration: 200 });
+  });
+
+  it('should call view.animate when zoomOut is called with map initialized', () => {
+    const viewSpy = jasmine.createSpyObj('View', ['getZoom', 'animate']);
+    spyOn(component.map, 'getView').and.returnValue(viewSpy);
+    viewSpy.getZoom.and.returnValue(5);
+
+    component.zoomOut();
+
+    expect(viewSpy.animate).toHaveBeenCalledWith({ zoom: 4, duration: 200 });
   });
 });
