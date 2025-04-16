322d3334c47f7d2adde27563e4f75a3b5e99714f
GeloBom <lghs60@hotmail.com>
2025-04-16 05:45:00 -0300

    feat: set up routing with AppComponent and add navigation structure

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { MapComponent } from './components/map/map.component';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [MapComponent],
+  imports: [MapComponent, RouterModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
